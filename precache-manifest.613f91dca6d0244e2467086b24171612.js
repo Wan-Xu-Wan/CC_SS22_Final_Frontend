e771f86ff2e5afd1d35672275ce60488b3707915
Wan Xu <91846138+Wan-Xu-Wan@users.noreply.github.com>
2022-08-16 20:53:51 -0400

    Add files via upload

---
+++ precache-manifest.613f91dca6d0244e2467086b24171612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee86022fc7e1bb2fc15c",
+    "url": "/static/css/main.8cc9b971.chunk.css"
+  },
+  {
+    "revision": "ee86022fc7e1bb2fc15c",
+    "url": "/static/js/main.1bb42dfe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3a9285fa548694e17978",
+    "url": "/static/js/2.7b470f55.chunk.js"
+  },
+  {
+    "revision": "355d0039458b0a971a4928a670f19432",
+    "url": "/static/media/upload.355d0039.svg"
+  },
+  {
+    "revision": "df8e597cad8c3bbd2589eaff49c0191c",
+    "url": "/static/media/mic.df8e597c.svg"
+  },
+  {
+    "revision": "6e63cd9494d970d65fff115d2b28422b",
+    "url": "/index.html"
+  }
+];
